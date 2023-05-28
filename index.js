a70ac70e7b0efd19c0fc18eb66ae11e85a7830a6
Dragon Dagger <zeddgrant@gmail.com>
2023-05-28 21:00:36 +1200

    think up possible function implementation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Hello world!");
+var robot = require("robotjs");
+
+// Attack a rat, Pick up rat bones and rat meat.
+const attackRat = () => {};
+
+// Bury the rat bones
+const buryBones = () => {};
+
+// Find a tree to cut for a log, light the log.
+const findTree = () => {};
+
+// Incorporate eating food to self-sustain ( for training with 1 Defence. )
+const eatFood = () => {};
+
+// Cook the rat meat, Drop any burnt meat.
+const cookFood = () => {};
