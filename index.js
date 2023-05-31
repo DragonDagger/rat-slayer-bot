22b9132f8928dab230698e9ef97e71fa4012f636
Dragon Dagger <zeddgrant@gmail.com>
2023-05-31 22:30:45 +1200

    MVP working as intended, attackrat and eat food if required

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const { sleep } = require("./functions/sleep");
 const { findRat } = require("./functions/findAll");
+const { findColor } = require("./functions/findColor");
 const robot = require("robotjs");
 
-// Attack a rat, Pick up rat bones and rat meat.
+// Incorporate eating food to self-sustain.
+const eatFood = () => {
+  // these are food colors for cooked rat meat
+  const foodColors = ["673806", "7a432f"];
+  // Look inside the inventory and eat some food, when at low health
+  findingFoodColor = findColor(1710, 745, 182, 254, foodColors);
+};
+
 const attackRat = () => {
   while (true) {
+    // find a rat and attack it
     findRat();
     sleep(12000);
+    // checks if heart icon health is low
     var img = robot.screen.capture(0, 0, 1920, 1080);
     var color = img.colorAt(1739, 86);
-    // checks if heart icon health is low
     if (color === "131313") {
-      robot.moveMouseSmooth(1820, 766, 0.5);
-      sleep(500);
-      robot.mouseClick();
+      eatFood();
     }
   }
 };
@@ -25,9 +32,6 @@ const buryBones = () => {};
 
 // Find a tree to cut for a log, light the log.
 const findTree = () => {};
-
-// Incorporate eating food to self-sustain.
-const eatFood = () => {};
 
 // Cook the rat meat, Drop any burnt meat.
 const cookFood = () => {
